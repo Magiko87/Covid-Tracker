4245eff59c3456dd5c10107fa0504cd039bbf60e
Daniele <>
2023-08-24 12:57:10 +0200

    bar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import routes from "../Routes/Routes";
+import routes from '../Routes/Routes';
+import DarkModeButton from '../DarkModeButton/DarkModeButton';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,20 +13,21 @@ const Navbar = () => {
 
   return (
     <nav className={`navbar ${isOpen ? 'open' : ''}`}>
-      <div className="menu-toggle" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+      <div className="navbar-content">
+        <DarkModeButton />
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        </div>
+        <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
+          {routes.map((route, index) => (
+            <li key={index}>
+              <Link to={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
-        {routes.map((route, index) => (
-          <li key={index}>
-            <Link to={route.path} onClick={toggleMenu}>
-              {route.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
     </nav>
   );
 };
